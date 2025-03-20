1c2c969ddc139ac4192bc756391c40913cfa6224
Doug Hirlinger <doughirlinger@gmail.com>
2025-03-20 10:57:23 -0400

    vite mod 4 complete

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,19 @@
 import logo from "../assets/GloboLogo.png"
+import styles from "./Banner.module.css"
+
+const subtitleStyle = {
+    fontStyle: "italic",
+    fontSize: "x-large",
+    color: "coral",
+  };
 
 const Banner = () => {
   return (
-    <header>
-      <div>
-        <img src={logo} alt="logo" />
+    <header className="row mb-4">
+      <div className="col-5">
+        <img src={logo} className={styles.logo} alt="logo" />
       </div>
-      <div>Providing houses all over the world</div>
+      <div className="col-7 mt-5" style={subtitleStyle}>Providing houses all over the world</div>
     </header>
   );
 }
